dfdd1b3ca1fb9024eba42957dfa719366c6c0cca
mbyczyns <mbyczyns@gmail.com>
2025-03-19 09:07:01 +0100

    some tests and fixes

--- classes.py
+++ classes.py
@@ -1,10 +1,10 @@
 class Article_snippet:
     def __init__(self, title, pub_date, main_image_url, print_headline, web_url, keywords):
-        self.title = title,
-        self.pub_date = pub_date,
-        self.main_image_url = main_image_url,
+        self.title = title
+        self.pub_date = pub_date
+        self.main_image_url = main_image_url
         self.print_headline = print_headline
-        self.web_url = web_url,
+        self.web_url = web_url
         self.keywords = keywords
     
     
--- testparser.py
+++ testparser.py
@@ -1,4 +1,5 @@
 from classes import Article_snippet
+from datetime import datetime
 import json
 
 def list_snippets(r):
@@ -6,35 +7,38 @@ def list_snippets(r):
     for i in r['response']['docs']:
         title = i['headline']['main']
         headline = i['headline']['print_headline']
-        pub_date = i['pub_date']
+        pub_date = extract_date(i['pub_date'])
         keywords = []
         for j in i['keywords']:
             keywords.append(j['value'])
         images=[]
         for j in i['multimedia']:
-            # print(j['url'])
             images.append(j['url'])
         if len(images)>0:
-            main_image_url = images[0]
+            main_image_url = "https://static01.nyt.com/" + images[0]
         else: main_image_url=''
-        print(f'------- length:{len(images)}----------')
         web_url = i['web_url']
-
-        print("----------------------------")
-        print(f"title: {title}, type: {type(title)}")
-        print(f"headline: {headline}, type: {type(headline)}")
-        print(f"pub_date: {pub_date}, type: {type(pub_date)}")
-        print(f"keywords: {keywords}, type: {type(keywords)}")
-        print(f"main_image_url: {main_image_url}, type: {type(main_image_url)}")
-        print(f"web_url: {web_url}, type: {type(web_url)}")
-        print("----------------------------")
 
         snippets.append(Article_snippet(title=title, print_headline=headline, pub_date=pub_date, keywords=keywords, main_image_url=main_image_url, web_url=web_url))
     return snippets
+
+def print_all(snippets):
+    for snip in snippets:
+        print(f"-------------{snip.title}------------")
+        print(snip.pub_date)
+        print(snip.keywords)
+        print(snip.main_image_url)
+        print(snip.print_headline)
+        print(snip.web_url)
+        print("--------------------------")
+
+def extract_date(rawdate):
+    dt = datetime.strptime(rawdate, "%Y-%m-%dT%H:%M:%S%z")
+    return dt.strftime("%Y-%m-%d")
 
 with open("json.json", "r") as f:
     js = json.load(f)
 
 parsed = list_snippets(js)
-for i in parsed:
-    print(type(i.title))
+print_all(parsed)
+    
